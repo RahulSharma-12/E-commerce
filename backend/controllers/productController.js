87ac1f149cc7d8595429d48325910b44d9e7f44a
Rahul Sharma <rahul1112sh@gmail.com>
2024-03-15 22:10:09 +0530

    Add Product route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
 const Product = require("../models/productModel");
-
+const ErrorHandler = require("../utils/errorhandler");
 
 
 exports.createProduct = async (req, res,next)=>{
@@ -10,8 +10,78 @@ exports.createProduct = async (req, res,next)=>{
     })
 }
 
+// Get All product
+exports.getAllProducts = async(req,res) =>{
+    const products = await Product.find();
 
-exports.getAllProducts = (req,res) =>{
 
-    res.status(200).json({message: "Route is working fine"});
+
+    res.status(200).json({
+        success :true,
+        products
+        });
 } 
+
+
+// Update product
+
+exports.updateProduct = async (req,res,next) =>{
+    let product = Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success : false,
+            message : "Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new: true,
+        runValidators : true,
+        useFindAndModify:false
+    });
+
+    res.status(200).json({
+        success:true,
+        product
+    }) 
+}
+
+
+// delete product
+
+exports.deleteProduct = async(req, res,next)=>{
+    const product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success : false,
+            message : "Product not found"
+        })
+    } 
+
+    await product.deleteOne();
+
+    res.status(200).json({
+        success:true,
+        message :"Product Delete Successfully"
+    })
+} 
+
+
+// Get Product Details
+
+exports.getProductDetails = async(req,res,next)=>{
+    const product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success : false, 
+            message : "Product not found",
+        });
+
+        // return next(new ErrorHandler("Product  Not Found", 404));
+    }  
+
+    res.status(200).json({ 
+        success:true,
+        product 
+    })
+}
